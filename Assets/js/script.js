82822bebca694dd9ae25794a7e374535eaba6b07
jaya4ever <111536082+jaya4ever@users.noreply.github.com>
2022-10-30 07:09:02 -0500

    working on time block

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -5,13 +5,15 @@ $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
 
 
-
+// clicking the save button for the time block
 $(".saveBtn").on("click", function(){
    var time = $(this).siblings(".hour").text();
    var textForDescription = $(this).siblings(".description").val();
    localStorage.setItem(textForDescription ,time)
 });
 
+
+//each time block shows the color if it is present, past or future
 
 
 
