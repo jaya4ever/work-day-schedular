c62b3a2d4059ba5e62be80f25c46c03a5d3d9b9f
jaya4ever <111536082+jaya4ever@users.noreply.github.com>
2022-10-30 06:06:30 -0500

    defined variable for save button

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,6 +3,7 @@
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
 
+var button = $(".saveBtn");
 
 
 
@@ -16,30 +17,3 @@ $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
 
 
-
-
-/*function createElements() {
-//TODO: Have to add AM and PM. 
-    for (var i = 9; i < 18; i++) {
-
-    // create the elements
-        var div = $('<div>');
-        var h6 = $('<h6>');
-        var textarea = $('<textarea>');
-        var button = $('<button>');
-
-   // add our classes
-        div.addClass("time-block row");
-        h6.addClass("hour col-1");
-        textarea.addClass("description col-10 red");
-        button.addClass("saveBtn col-1");
-        
-       h6.text(i)
-        button.text('save')
-
-        // append to the container
-        div.append(h6, textarea, button);
-        $('.container').append(div);
-    }
-
-}createElements()*/
