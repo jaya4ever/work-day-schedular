1f59167f2f6f5a6686214ab8a0008f4012ed48de
jaya4ever <111536082+jaya4ever@users.noreply.github.com>
2022-10-30 05:12:39 -0500

    commented out for loop script and working on index.html

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,29 +1,40 @@
 
-
-
+// cureent day and time at the top of the page
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
-function createElements() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function createElements() {
 //TODO: Have to add AM and PM. 
     for (var i = 9; i < 18; i++) {
 
-       
-        // create the elements
+    // create the elements
         var div = $('<div>');
         var h6 = $('<h6>');
         var textarea = $('<textarea>');
         var button = $('<button>');
 
-        // add our classes
+   // add our classes
         div.addClass("time-block row");
         h6.addClass("hour col-1");
         textarea.addClass("description col-10 red");
         button.addClass("saveBtn col-1");
         
-        
-        
-
-        h6.text(i)
+       h6.text(i)
         button.text('save')
 
         // append to the container
@@ -31,6 +42,4 @@ function createElements() {
         $('.container').append(div);
     }
 
-}
-
-createElements()
+}createElements()*/
