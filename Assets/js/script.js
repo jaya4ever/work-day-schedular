1d818bff01f4ab625972739dec844ac854050575
jaya4ever <111536082+jaya4ever@users.noreply.github.com>
2022-10-30 06:49:06 -0500

    modified script and added attribute used in jQuery

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,12 +3,14 @@
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
 
-var savebutton = $(".saveBtn");
 
 
-savebutton.on("click", function(){
 
-});
+$(".saveBtn").on("click", function(){
+   var descriptionForText = $(this).siblings(".description").val();
+   var time = $(this).parent().attr("id")
+   localStorage.setItem(descriptionForText ,time)
+})
 
 
 
