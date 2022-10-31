316c16e9a0f270afaaf9f942c79476dddca20898
jaya4ever <111536082+jaya4ever@users.noreply.github.com>
2022-10-31 06:15:04 -0500

    started debugging and managing functions

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,75 +4,81 @@ $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
 
 
-//Debugging: defining save button
-//var saveBtn = $(".saveBtn");
+//defining save button(do I need to define it like this in jQuerry or just use $(".saveBtn") at line 11?)
+var saveButton = $(".saveBtn");
 
 // clicking the save button for the time block
-$(".saveBtn").on("click", function(){
-   var time = $(this).siblings(".hour").text();
+/*saveButton.on("click", function () {
+    var time = $(this).siblings(".hour").text();
 
-   var textForDescription = $(this).siblings(".description").val();
+    var textForDescription = $(this).siblings(".description").val();
 
-   localStorage.setItem(time,textForDescription );
-});
+    localStorage.setItem(time, textForDescription);
+});*/
 
 
 //each time block shows the color if it is present, past or future
- //var currentTime = moment().hours();
-
-  function timeblockForColor(){
+function timeblockForColor() {
 
     // this one get the current number of hours
     var currentTime = moment().hours();
 
 
     // using loop for time blocks for past present and future as they change color accordingly 
-    $(".time-block").each(function(){
+    $(".time-block").each(function () {
 
         var timeBlock = parseInt($(this).attr("id"));
         //var inHour = moment($(this).attr("id"), ["hA"]).hour()
-       // var inHour = $(this).data('hour')
+        // var inHour = $(this).data('hour')
 
-      //TODO: do i need to remove class each time?
-        if(timeBlock < currentTime ){
+        //TODO: do i need to remove class each time?
+        if (timeBlock < currentTime) {
             $(this).addClass("past");
 
-        }else if (timeBlock > currentTime){
+        } else if (timeBlock > currentTime) {
             $(this).removeClass("past");
             $(this).addClass("future");
 
-            
-        }else{
+
+        } else {
             $(this).removeClass("future");
             $(this).removeClass("past");
             $(this).addClass("present");
         }
     })
-    
-  };
-  
+
+};
+// clicking the save button for the time block
+saveButton.on("click", function () {
+    var time = $(this).siblings(".hour").text();
+
+    var textForDescription = $(this).siblings(".description").val();
+
+    localStorage.setItem(time, textForDescription);
+});
 
 
-  //after refreshing page i see if saved events is there
-     function saveEventToUse(){
-        $(".hour").each(function(){
 
-            var currentHour =  $(this).text();
-            var currentPlan = localStorage.getItem(currentHour);
+//after refreshing page i see if saved events is there
+function saveEventToUse() {
+    $(".hour").each(function () {
+
+        var currentHour = $(this).text();
+        var currentPlan = localStorage.getItem(currentHour);
 
 
-            if(currentPlan !== null){
-                $(this).siblings(".description").val(currentPlan);
-            }
+        if (currentPlan !== null) {
+            $(this).siblings(".description").val(currentPlan);
+        }
 
-        });
-     }
-    
-     
+    });
+}
 
-     //calling functions
-     timeblockForColor();
-     saveEventToUse();
+
+
+//calling functions
+timeblockForColor();
+saveEventToUse();
 
 
 
