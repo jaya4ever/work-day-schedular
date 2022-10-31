b3dedf73d16b69149de001c79e931e600b28a8c7
jaya4ever <111536082+jaya4ever@users.noreply.github.com>
2022-10-31 06:31:07 -0500

    testing it while adding and removing scripts

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -8,13 +8,13 @@ $("#currentDay").text(moment().format("dddd, MMMM Do"));
 var saveButton = $(".saveBtn");
 
 // clicking the save button for the time block
-/*saveButton.on("click", function () {
+saveButton.on("click", function () {
     var time = $(this).siblings(".hour").text();
 
     var textForDescription = $(this).siblings(".description").val();
 
     localStorage.setItem(time, textForDescription);
-});*/
+});
 
 
 //each time block shows the color if it is present, past or future
@@ -48,19 +48,12 @@ function timeblockForColor() {
     })
 
 };
-// clicking the save button for the time block
-saveButton.on("click", function () {
-    var time = $(this).siblings(".hour").text();
-
-    var textForDescription = $(this).siblings(".description").val();
-
-    localStorage.setItem(time, textForDescription);
-});
 
 
 
-//after refreshing page i see if saved events is there
-function saveEventToUse() {
+
+//after refreshing page i see saved events is there
+function saveEvents() {
     $(".hour").each(function () {
 
         var currentHour = $(this).text();
@@ -78,7 +71,7 @@ function saveEventToUse() {
 
 //calling functions
 timeblockForColor();
-saveEventToUse();
+saveEvents();
 
 
 
